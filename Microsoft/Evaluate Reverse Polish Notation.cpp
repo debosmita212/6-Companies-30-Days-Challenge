9800ec719232b832409607a991080d7d9ad8ca4e
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-01-01 22:24:12 +0530

    Create Evaluate Reverse Polish Notation.cpp

---
+++ Microsoft/Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+  //Evaluating postfix expression
+    long long evalRPN(vector<string>& tokens) {
+        stack<long long> st;
+        int n=tokens.size();
+        for(int i=0;i<n;i++){
+          //atleast two numbers should be there to evaluate the expression
+            if(st.size()>1 && (tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/")){
+              //operand order 
+                long long op2=st.top(); st.pop();
+                long long op1=st.top(); st.pop();
+                if(tokens[i]=="+"){
+                    long long res=op1+op2;
+                    st.push(res);
+                }
+                else if(tokens[i]=="-"){
+                    long long res=op1-op2;
+                    st.push(res);
+                }
+                else if(tokens[i]=="*"){
+                    long long res=op1*op2;
+                    st.push(res);
+                }
+                else if(tokens[i]=="/"){
+                    long long res=op1/op2;
+                    st.push(res);
+                }
+            }
+            else{
+              //numbers push in stack
+                st.push(stoi(tokens[i]));
+            }
+        }
+        return st.top();
+    }
+};
