d3b049a04ead61363b5def34e5888cb4ac121dfa
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-01-07 11:50:20 +0530

    Create  Airplane Seat Assignment Probability.cpp

---
+++ Microsoft/Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,11 @@
+//Leetcode 1227
+class Solution {
+public:
+    /*
+    Since the person has two choices either sit on his chair or not
+    */
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1) return 1;
+        return 0.5;
+    }
+};
