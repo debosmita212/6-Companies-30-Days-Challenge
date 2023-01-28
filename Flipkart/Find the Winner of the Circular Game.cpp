4e257b68a4410dfc057f8d83e7425b99ee3287f5
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-01-28 23:18:09 +0530

    Create Find the Winner of the Circular Game.cpp

---
+++ Flipkart/Find the Winner of the Circular Game.cpp
@@ -0,0 +1,14 @@
+//Leetcode 1823
+class Solution {
+public:
+    int solve(int n,int k){
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=(ans+k)%i;
+        }
+        return ans;
+    }
+    int findTheWinner(int n, int k) {
+        return solve(n,k)+1; //for 1 based indexing adding 1
+    }
+};
